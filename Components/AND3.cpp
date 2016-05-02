3e539798f776426b0709e7191cd2d389ba3eecbe
hemoali <i.radwan1996@gmail.com>
2016-05-02 19:10:36 +0200

    Fix drawing AND3

--- Components/AND3.cpp
+++ Components/AND3.cpp
@@ -19,7 +19,7 @@ void AND3::Operate()
 // Draws 3-input AND gate
 void AND3::Draw(Output* pOut, bool highlight)
 {
-	if (!getDelete()) pOut->DrawAnd_Nand(m_CenterInfo,3, true, highlight);
+	if (!getDelete()) pOut->DrawAnd_Nand(m_CenterInfo,3, false, highlight);
 	else {
 		pOut->DrawCleanImage(getSmallCleanImageBeforeAddingComp(), m_CenterInfo.x1, m_CenterInfo.y1);
 	}
